0d9781f1221b64547a2643bd0146ca8d49c07b20
DoniyorZ <85928570+DoniyorZ@users.noreply.github.com>
2021-07-29 14:09:28 +0500

    Delete ramenpos/Pods directory

--- ramenpos/Pods/Target Support Files/Pods-RamenPosTests/Pods-RamenPosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RamenPosTests : NSObject
-@end
-@implementation PodsDummy_Pods_RamenPosTests
-@end
--- ramenpos/Pods/Target Support Files/SPIClient-iOS/SPIClient-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SPIClient_iOS : NSObject
-@end
-@implementation PodsDummy_SPIClient_iOS
-@end
